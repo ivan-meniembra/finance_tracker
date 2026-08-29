56a7b4a96513c38b083eebbd4b6aff9f9b59c907
ivan-meniembra <lemeniembra@gmail.com>
2026-08-29 10:43:06 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fintrack-v3';
+const CACHE = 'fintrack-v4';
 const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (e) => {
